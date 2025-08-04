bb79b30801366dac0e6f7500e4f62ee5810751ad
jadenwilliams26 <jadendwi6@gmail.com>
2025-08-03 22:55:31 -0600

    Module 5 completed baseline

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -5,7 +5,29 @@ const Model = mongoose.model('trips');
 // GET: /trips - lists all the trips
 // Regardless of outcome, response must include HTML status code
 // and JSON message to the request client
-const tripsList = async(requestAnimationFrame, res) => {
+const tripsList = async(req, res) => {
+    const q = await Model
+        .find({}) // No filter, return all records
+        .exec();
+
+        // Uncomment the following line to show results of querey
+        // on the console
+        //console.log(q);
+
+    if(!q)
+    { // Database returned no data
+        return res
+                .status(404)
+                .json(err);
+    } else { // Return resulting trip list
+        return res
+            .status(200)
+            .json(q);
+    }
+
+};
+
+const tripsFindByCode = async(req, res) => {
     const q = await Model
         .find({'code' : req.params.tripCode}) // Return single record
         .exec();
